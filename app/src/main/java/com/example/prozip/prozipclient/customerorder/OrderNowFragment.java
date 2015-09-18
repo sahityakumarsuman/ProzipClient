f0991892aa4b740eb8b3f56fff72606a66129b7a
sahityakumarsuman <sahityakumarsuman@gmail.com>
2015-09-18 23:50:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/prozip/prozipclient/customerorder/OrderNowFragment.java
@@ -0,0 +1,7 @@
+package com.example.prozip.prozipclient.customerorder;
+
+/**
+ * Created by Duke on 9/18/2015.
+ */
+public class OrderNowFragment {
+}
---
+++ app/src/main/java/com/example/prozip/prozipclient/login/SignInFragment.java
@@ -0,0 +1,7 @@
+package com.example.prozip.prozipclient.login;
+
+/**
+ * Created by Duke on 9/18/2015.
+ */
+public class SignInFragment {
+}
